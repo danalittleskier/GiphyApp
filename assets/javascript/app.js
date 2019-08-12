5feab6777f482cb3f0d6324153bcf601e651e59d
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-08-11 18:38:10 -0600

    Changed buttons style. Updated gif animation vs still. More responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,85 +1,103 @@
- var natureArray = ["Beaches","Mountains"];
+var topics = ["Sandy Beach", "Mountain", "Surfing", "Skiing"];
 
- function renderButtons() {
+renderButtons();
+
+function renderButtons() {
 
   // Deleting the buttons prior to adding new buttons
   // (this is necessary otherwise we will have repeat buttons)
   $("#buttons-view").empty();
 
-  // Looping through the array
-  for (var i = 0; i < natureArray.length; i++) {
+  for (var i = 0; i < topics.length; i++) {
 
-    // Then dynamicaly generating buttons for each movie in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
     var a = $("<button>");
-    // Adding a class
-    a.addClass("nature");
-    // Adding a data-attribute with a value of the movie at index i
-    a.attr("data-term", natureArray[i]);
-    // Providing the button's text with a value of the movie at index i
-    a.text(natureArray[i]);
+    a.addClass("btn btn-secondary nature");
+    a.attr("data-term", topics[i]);
+    a.text(topics[i]);
     // Adding the button to the HTML
     $("#buttons-view").append(a);
   }
 }
- // Event listener for all button elements
- $("button").on("click", function(event) {
-    event.preventDefault();
-    
-    var natureTerm = $(this).attr("data-term");
-    console.log(natureTerm);
-    // Constructing a URL to search Giphy for nature term
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      natureTerm + "&api_key=3HPY5IJdMQEsEptdLFxbSXs6eC04Mr7q&limit=10";
 
-    // Performing our AJAX GET request
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After the data comes back from the API store the response
-      .then(function(response) {
-        var results = response.data;
-        
-        $("#gifs-appear-here").empty();
+// Event listener for all button elements
+//$("button").on("click", function(event) {
+$(document).on("click", "button", function () {
+  event.preventDefault();
 
-        for (var i = 0; i < results.length; i++) {
+  var natureTerm = $(this).attr("data-term");
 
-          // Only taking action if the photo has an appropriate rating
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
-            
-            var p = $("<p>").text("Rating: " + rating);
-            var natureImage = $("<img>");
+  // Constructing a URL to search Giphy for nature term
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    natureTerm + "&api_key=3HPY5IJdMQEsEptdLFxbSXs6eC04Mr7q&limit=10";
 
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            natureImage.attr("src", results[i].images.fixed_height.url);
+  // Performing our AJAX GET request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After the data comes back from the API store the response
+    .then(function (response) {
+      var results = response.data;
 
-            gifDiv.append(p);
-            gifDiv.append(natureImage);
+      $("#gifs-appear-here").empty();
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
+      for (var i = 0; i < results.length; i++) {
+
+        // Only taking action if the photo has an appropriate rating
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          var gifDiv = $("<div>");
+          gifDiv.addClass("col-sm")
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+          var natureImage = $("<img>");
+
+          // Giving the image tag an src attribute of a proprty pulled off the
+          // result item
+          natureImage.attr("src", results[i].images.fixed_height_still.url);
+          natureImage.attr("data-still", results[i].images.fixed_height_still.url);
+          natureImage.attr("data-state", "still");
+          natureImage.attr("data-animate", results[i].images.fixed_height.url);
+          natureImage.addClass("gif");
+
+          gifDiv.append(natureImage);
+          gifDiv.append(p);
+
+          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+          $("#gifs-appear-here").append(gifDiv);
         }
-      });
-  });
+      }
+    });
+});
 
-  // This function handles events where the add search button is clicked
-  $("#add-term-button").on("click", function(event) {
-    event.preventDefault();
-    // This line of code will grab the input from the textbox
-    $("#gifs-appear-here").empty();
+$(document).on("click", ".gif", function () {
+  
+  var state = $(this).attr("data-state");
+  console.log("state " + state);
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
 
-    var subject = $("#nature-input").val().trim();
-    natureArray.push(subject);
-    
-    // Calling renderButtons which handles the processing of our term array
-    renderButtons();
+// This function handles events where the add search button is clicked
+$(document).on("click", "#add-term-button", function () {
+  event.preventDefault();
+  $("#gifs-appear-here").empty();
 
-  });
-
+  var topic = $("#nature-input").val().trim();
+  if (topic !== "") {
+    topics.push(topic);
+  }
+  // Calling renderButtons which handles the processing of our term array
   renderButtons();
+
+});
+
+
